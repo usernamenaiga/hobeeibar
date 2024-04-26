df8e19b6fcd6c77e940547359c0b3e5fdf5b2f36
themainbot <166839882+themainbot@users.noreply.github.com>
2024-04-27 00:40:09 +0600

    Update approve.js

--- scripts/commands/approve.js
+++ scripts/commands/approve.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "approve",
   version: "2.0.0",
   permission: 0,
-  credits: "nayan",
+  credits: "Limon",
   description: "approve thread using thread id",
   prefix: false,
   category: "admin",
